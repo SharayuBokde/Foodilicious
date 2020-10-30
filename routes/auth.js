3ae55ecb3bf47f1ab692d31d57ecc65f4885d211
Sharayu Bokde <bokde.sharayu@gmail.com>
2020-10-30 23:21:28 +0530

    added review

--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/register',authController.register);
 router.post('/login',authController.login);
 router.get('/subCategory/:id',dataController.subCategory);
-
+router.post('/postReview', dataController.postReview);
+router.get('/getReview/:id', dataController.getReview);
 module.exports = router; 
